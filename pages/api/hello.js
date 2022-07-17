a665e09a662aa3f6e06b7b02d6eb2520005a75b9
zerogravitymeat <zerogravitymeat@gmail.com>
2022-07-17 20:27:27 +0900

    prisma 연동 완료

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,9 @@
 import pool from '../../config/db';
 
 export default function handler(req, res) {
-  pool.query("select * from kccu.USERS", function (err, result) {
+  pool.query("select * from kccu.Manager;", function (err, result) {
     if (err) throw err;
     console.log(result);
-    res.status(200).json({ name: result[0].USER_NAME })
+    res.status(200).json({ name: result[1].name })
   });
 }
